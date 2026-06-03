7790468dc5d482a2e1f20da666f0244b2f973f8e
Bjornhona <asaeri3@hotmail.com>
2026-06-03 11:36:14 +0200

    loding added while checked if registered

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </Suspense>
+  );
 }
