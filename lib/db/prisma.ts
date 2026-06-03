ac2526724608c2171ce21e38ba954e239833dfc7
Bjornhona <asaeri3@hotmail.com>
2026-06-03 17:52:02 +0200

    GET and POST categories created and the categories page

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,15 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as {
-    prisma : PrismaClient | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma = 
-globalForPrisma.prisma ?? 
-new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query','error','warn'] : ['error'],
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
 });
 
-if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,26 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-    prisma : PrismaClient | undefined;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
